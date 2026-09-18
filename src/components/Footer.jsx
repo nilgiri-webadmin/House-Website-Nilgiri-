2f6c8bdf5d920f40274d0505d6c19776f7d48e10
25f3002130 <25f3002130@ds.study.iitm.ac.in>
2026-09-18 20:42:54 +0530

    fixed zincxnh link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
                             <li><a href="https://docs.google.com/document/u/1/d/e/2PACX-1vRxGnnDCVAO3KX2CGtMIcJQuDrAasVk2JHbDxkjsGrTP5ShhZK8N6ZSPX89lexKx86QPAUswSzGLsOA/pub" target="_blank" rel="noreferrer">Student Handbook <ExternalLink size={12} /></a></li>
                             <li><a href="https://forms.gle/NQkWj5p8146CPuCBA" target="_blank" rel="noreferrer">Grievance Redressal <ExternalLink size={12} /></a></li>
                             <li><a href="https://drive.google.com/file/d/1_vslwUdBNFCH6DSTLeVlqvX5Rtw57gSD/view" target="_blank" rel="noreferrer">Academic Calendar <ExternalLink size={12} /></a></li>
-                            <li><a href="https://zincxnh.vercel.app/" target="_blank" rel="noreferrer">ZINCxNH <ExternalLink size={12} /></a></li>
+                            <li><a href="https://zin-cx-nh-jade.vercel.app/" target="_blank" rel="noreferrer">ZINCxNH <ExternalLink size={12} /></a></li>
                         </ul>
                     </div>
 
